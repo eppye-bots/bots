cf0a1117ef9d6baf6f9ee9d709c244f92c7d098e
hjebbers <hjebbers@gmail.com>
2010-08-02 23:51:11 +0200

    version 2.0.1

--- bots/botsglobal.py
+++ bots/botsglobal.py
@@ -3,6 +3,6 @@ incommunicate = False    #used to set all incommuncation off
 db = None               #db-object
 ini = None              #ini-file-object that is read (bots.ini)
 routeid = ''            #current route. This is used to set routeid for Processes.
-version = '2.0.0'       #bots version
+version = '2.0.1'       #bots version
 minta4query = 0           #used in retry; this determines which ta's are queryed in a route
 ######################################
