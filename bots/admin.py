050d3f0eca7cf06f1c1022778ec742bded4e316d
hjebbers <hjebbers@gmail.com>
2011-08-03 12:10:07 +0200

    set the right ordering explicit in admin; set ordering to multiple fields, in development version (?1.4?) this seems to work. sorted meta fields.

--- bots/admin.py
+++ bots/admin.py
@@ -54,11 +54,11 @@ class BotsAdmin(admin.ModelAdmin):
 
 #*****************************************************************************************************
 class CcodeAdmin(BotsAdmin):
+    actions = ('bulk_delete',)
     list_display = ('ccodeid','leftcode','rightcode','attr1','attr2','attr3','attr4','attr5','attr6','attr7','attr8')
     list_display_links = ('ccodeid',)
     list_filter = ('ccodeid',)
-    #~ ordering = ('ccodeid','leftcode')
-    actions = ('bulk_delete',)
+    ordering = ('ccodeid','leftcode')
     search_fields = ('leftcode','rightcode','attr1','attr2','attr3','attr4','attr5','attr6','attr7','attr8')
     def lookup_allowed(self, lookup, *args, **kwargs):
         if lookup.startswith('ccodeid'):
@@ -67,16 +67,17 @@ class CcodeAdmin(BotsAdmin):
 admin.site.register(models.ccode,CcodeAdmin)
 
 class CcodetriggerAdmin(BotsAdmin):
+    actions = ('bulk_delete',)
     list_display = ('ccodeid','ccodeid_desc',)
     list_display_links = ('ccodeid',)
-    actions = ('bulk_delete',)
+    ordering = ('ccodeid',)
 admin.site.register(models.ccodetrigger,CcodetriggerAdmin)
 
 class ChannelAdmin(BotsAdmin):
+    actions = ('bulk_delete',)
     list_display = ('idchannel', 'inorout', 'type','host', 'port', 'username', 'secret', 'path', 'filename',  'remove', 'archivepath', 'charset')
     list_filter = ('inorout','type')
-    #~ ordering = ('idchannel',)
-    actions = ('bulk_delete',)
+    ordering = ('idchannel',)
     fieldsets = (
         (None,          {'fields': ('idchannel', ('inorout','type'), ('host','port'), ('username', 'secret'), ('path', 'filename'), 'remove', 'archivepath', 'charset','desc')
                         }),
@@ -90,11 +91,11 @@ class ChannelAdmin(BotsAdmin):
 admin.site.register(models.channel,ChannelAdmin)
 
 class ConfirmruleAdmin(BotsAdmin):
+    actions = ('activate','bulk_delete')
     list_display = ('active','negativerule','confirmtype','ruletype', 'frompartner', 'topartner','idroute','idchannel','editype','messagetype')
     list_display_links = ('confirmtype',)
     list_filter = ('active','confirmtype','ruletype')
-    actions = ('activate','bulk_delete')
-    #~ ordering = ('confirmtype','ruletype')
+    ordering = ('confirmtype','ruletype')
     fieldsets = (
         (None, {'fields': ('active','negativerule','confirmtype','ruletype','frompartner', 'topartner','idroute','idchannel',('editype','messagetype'))}),
         )
@@ -116,22 +117,24 @@ class MyPartnerAdminForm(django.forms.ModelForm):
         return self.cleaned_data
 
 class PartnerAdmin(BotsAdmin):
+    actions = ('bulk_delete','activate')
     form = MyPartnerAdminForm
-    inlines = (MailInline,)
     fields = ('active', 'isgroup', 'idpartner', 'name','mail','cc','group')
+    filter_horizontal = ('group',)
+    inlines = (MailInline,)
     list_display = ('active','isgroup','idpartner', 'name','mail','cc')
     list_display_links = ('idpartner',)
     list_filter = ('active','isgroup')
-    filter_horizontal = ('group',)
-    actions = ('bulk_delete','activate')
+    ordering = ('idpartner',)
     search_fields = ('idpartner','name')
 admin.site.register(models.partner,PartnerAdmin)
 
 class RoutesAdmin(BotsAdmin):
+    actions = ('bulk_delete','activate')
     list_display = ('active', 'idroute', 'seq', 'fromchannel', 'fromeditype', 'frommessagetype', 'alt', 'frompartner', 'topartner', 'translateind', 'tochannel', 'defer', 'toeditype', 'tomessagetype', 'frompartner_tochannel', 'topartner_tochannel', 'testindicator', 'notindefaultrun')
     list_display_links = ('idroute',)
     list_filter = ('idroute','active','fromeditype','testindicator')
-    actions = ('bulk_delete','activate')
+    ordering = ('idroute','seq')
     fieldsets = (
         (None,      {'fields':  ('active',('idroute', 'seq'),'fromchannel', ('fromeditype', 'frommessagetype'),'translateind','tochannel','desc')}),
         (_(u'Filtering for outchannel'),{'fields':('toeditype', 'tomessagetype','frompartner_tochannel', 'topartner_tochannel', 'testindicator'),
@@ -159,11 +162,12 @@ class MyTranslateAdminForm(django.forms.ModelForm):
         return self.cleaned_data
 
 class TranslateAdmin(BotsAdmin):
+    actions = ('bulk_delete','activate')
     form = MyTranslateAdminForm
     list_display = ('active', 'fromeditype', 'frommessagetype', 'alt', 'frompartner', 'topartner', 'tscript', 'toeditype', 'tomessagetype')
     list_display_links = ('fromeditype',)
     list_filter = ('active','fromeditype','toeditype')
-    actions = ('bulk_delete','activate')
+    ordering = ('fromeditype','frommessagetype')
     fieldsets = (
         (None,      {'fields': ('active', ('fromeditype', 'frommessagetype'),'tscript', ('toeditype', 'tomessagetype','desc'))
                     }),
@@ -174,10 +178,11 @@ class TranslateAdmin(BotsAdmin):
 admin.site.register(models.translate,TranslateAdmin)
 
 class UniekAdmin(BotsAdmin):     #AKA counters
+    actions = None
     list_display = ('domein', 'nummer')
     list_editable = ('nummer',)
-    #~ ordering = ('domein',)
-    actions = None
+    ordering = ('domein',)
+    search_fields = ('domein',)
 admin.site.register(models.uniek,UniekAdmin)
 
 #User - change the default display of user screen
