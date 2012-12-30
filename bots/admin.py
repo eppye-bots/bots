729f1881388f1181fc35893006418d281e8beb3e
hjebbers <hjebbers@gmail.com>
2012-12-30 15:15:52 +0100

    - layout changes in admin - added extra checks for confirmations and routes - removed editype for confirmations

--- bots/admin.py
+++ bots/admin.py
@@ -47,29 +47,56 @@ class ChannelAdmin(BotsAdmin):
     ordering = ('idchannel',)
     search_fields = ('idchannel', 'inorout', 'type','host', 'username', 'path', 'filename', 'archivepath')
     fieldsets = (
-        (None,          {'fields': (('idchannel', 'inorout', 'type'), 'remove', ('host','port'), ('username', 'secret'), ('path', 'filename'), 'archivepath', 'desc')
+        (None,          {'fields': (('idchannel', 'inorout', 'type'), 'remove', ('host','port'), ('username', 'secret'), ('path', 'filename'), 'archivepath', 'desc'),
+                         'classes': ('wide extrapretty',)
                         }),
         (_(u'Email specific'),{'fields': ('starttls', 'apop', 'askmdn', 'sendmdn' ),
-                         'classes': ('collapse',)
+                         'classes': ('collapse wide extrapretty',)
                         }),
         (_(u'FTP specific'),{'fields': ('ftpactive', 'ftpbinary', 'ftpaccount' ),
-                         'classes': ('collapse',)
+                         'classes': ('collapse wide extrapretty',)
                         }),
-        (_(u'Advanced'),{'fields': ('syslock', 'lockname', 'parameters', 'rsrv2', 'keyfile', 'certfile'),
-                         'classes': ('collapse',)
+        (_(u'Advanced'),{'fields': ('rsrv2', 'syslock', 'lockname', 'keyfile', 'certfile', 'parameters'),
+                         'classes': ('collapse wide extrapretty',)
                         }),
     )
 admin.site.register(models.channel,ChannelAdmin)
 
+class MyConfirmruleAdminForm(django.forms.ModelForm):
+    ''' customs form for route for additional checks'''
+    class Meta:
+        model = models.confirmrule
+    def clean(self):
+        super(MyConfirmruleAdminForm, self).clean()
+        if self.cleaned_data['ruletype'] == 'route':
+            if not self.cleaned_data['idroute']:
+                raise django.forms.util.ValidationError(_(u'For ruletype "route" it is required to indicate a route.'))
+        elif self.cleaned_data['ruletype'] == 'channel':
+            if not self.cleaned_data['idchannel']:
+                raise django.forms.util.ValidationError(_(u'For ruletype "channel" it is required to indicate a channel.'))
+        elif self.cleaned_data['ruletype'] == 'frompartner':
+            if not self.cleaned_data['frompartner']:
+                raise django.forms.util.ValidationError(_(u'For ruletype "frompartner" it is required to indicate a frompartner.'))
+        elif self.cleaned_data['ruletype'] == 'topartner':
+            if not self.cleaned_data['topartner']:
+                raise django.forms.util.ValidationError(_(u'For ruletype "topartner" it is required to indicate a topartner.'))
+        elif self.cleaned_data['ruletype'] == 'messagetype':
+            if not self.cleaned_data['messagetype']:
+                raise django.forms.util.ValidationError(_(u'For ruletype "messagetype" it is required to indicate a messagetype.'))
+        return self.cleaned_data
+
 class ConfirmruleAdmin(BotsAdmin):
     actions = ('activate',)
-    list_display = ('active','negativerule','confirmtype','ruletype', 'frompartner', 'topartner','idroute','idchannel','editype','messagetype')
+    form = MyConfirmruleAdminForm
+    list_display = ('active','negativerule','confirmtype','ruletype', 'frompartner', 'topartner','idroute','idchannel','messagetype')
     list_display_links = ('confirmtype',)
     list_filter = ('active','confirmtype','ruletype')
-    search_fields = ('confirmtype','ruletype', 'frompartner__idpartner', 'topartner__idpartner', 'idroute', 'idchannel__idchannel', 'editype', 'messagetype')
+    search_fields = ('confirmtype','ruletype', 'frompartner__idpartner', 'topartner__idpartner', 'idroute', 'idchannel__idchannel', 'messagetype')
     ordering = ('confirmtype','ruletype')
     fieldsets = (
-        (None, {'fields': ('active','negativerule','confirmtype','ruletype','frompartner', 'topartner','idroute','idchannel',('editype','messagetype'))}),
+        (None, {'fields': ('active','negativerule','confirmtype','ruletype','frompartner', 'topartner','idroute','idchannel','messagetype'),
+                'classes': ('wide extrapretty',)
+               }),
         )
 admin.site.register(models.confirmrule,ConfirmruleAdmin)
 
@@ -99,34 +126,48 @@ class PartnerAdmin(BotsAdmin):
     ordering = ('idpartner',)
     search_fields = ('idpartner','name','mail','cc','attr1','attr2','attr3','attr4','attr5','name1','name2','name3')
     fieldsets = (
-        (None,          {'fields': (('idpartner', 'active', 'isgroup'), 'name', ('mail','cc'),'desc',('startdate', 'enddate'))
+        (None,          {'fields': ('active', ('idpartner', 'isgroup'), 'name', ('mail','cc'),'desc',('startdate', 'enddate')),
+                         'classes': ('wide extrapretty',)
                         }),
         (_(u'Address'),{'fields': ('name1','name2','name3','address1','address2','address3',('postalcode','city'),('countrycode','countrysubdivision'),('phone1','phone2')),
-                         'classes': ('collapse',)
+                         'classes': ('collapse wide extrapretty',)
                         }),
         (_(u'Is in groups'),{'fields': ('group',),
-                         'classes': ('collapse',)
+                         'classes': ('collapse wide extrapretty',)
                         }),
         (_(u'User defined'),{'fields': ('attr1','attr2','attr3','attr4','attr5'),
-                         'classes': ('collapse',)
+                         'classes': ('collapse wide extrapretty',)
                         }),
     )
 admin.site.register(models.partner,PartnerAdmin)
 
+class MyRouteAdminForm(django.forms.ModelForm):
+    ''' customs form for route for additional checks'''
+    class Meta:
+        model = models.routes
+    def clean(self):
+        super(MyRouteAdminForm, self).clean()
+        if self.cleaned_data['fromchannel'] and (not self.cleaned_data['fromeditype'] or not self.cleaned_data['frommessagetype']):
+            raise django.forms.util.ValidationError(_(u'When using an inchannel both "fromeditype" and "frommessagetype" are required.'))
+        return self.cleaned_data
+
 class RoutesAdmin(BotsAdmin):
     actions = ('activate',)
+    form = MyRouteAdminForm
     list_display = ('active','idroute','seq','fromchannel','fromeditype','frommessagetype','alt','frompartner','topartner','translt','tochannel','defer','toeditype','tomessagetype','frompartner_tochannel','topartner_tochannel','testindicator','notindefaultrun','zip_incoming','zip_outgoing')
     list_display_links = ('idroute',)
     list_filter = ('idroute','active','fromeditype')
     ordering = ('idroute','seq')
     search_fields = ('idroute', 'fromchannel__idchannel','fromeditype', 'frommessagetype', 'alt', 'tochannel__idchannel','toeditype', 'tomessagetype')
     fieldsets = (
-        (None,      {'fields':  ('active',('idroute', 'seq'),'fromchannel', ('fromeditype', 'frommessagetype'),'translateind','tochannel','desc')}),
+        (None,      {'fields':  ('active',('idroute', 'seq'),'fromchannel', ('fromeditype', 'frommessagetype'),'translateind','tochannel','desc'),
+                     'classes': ('wide extrapretty',)
+                    }),
         (_(u'Filtering for outchannel'),{'fields':('toeditype', 'tomessagetype','frompartner_tochannel', 'topartner_tochannel', 'testindicator'),
-                    'classes':  ('collapse',)
+                    'classes':  ('collapse wide extrapretty',)
                     }),
         (_(u'Advanced'),{'fields':  ('alt','frompartner','topartner','notindefaultrun','defer','zip_incoming','zip_outgoing'),
-                     'classes': ('collapse',)
+                     'classes': ('collapse wide extrapretty',)
                     }),
     )
 admin.site.register(models.routes,RoutesAdmin)
@@ -155,10 +196,11 @@ class TranslateAdmin(BotsAdmin):
     ordering = ('fromeditype','frommessagetype')
     search_fields = ('fromeditype', 'frommessagetype', 'alt', 'frompartner__idpartner', 'topartner__idpartner', 'tscript', 'toeditype', 'tomessagetype')
     fieldsets = (
-        (None,      {'fields': ('active', ('fromeditype', 'frommessagetype'),'tscript', ('toeditype', 'tomessagetype','desc'))
+        (None,      {'fields': ('active', ('fromeditype', 'frommessagetype'),'tscript', ('toeditype', 'tomessagetype'),'desc'),
+                     'classes': ('wide extrapretty',)
                     }),
         (_(u'Multiple translations per editype/messagetype'),{'fields': ('alt', 'frompartner', 'topartner'),
-                     'classes': ('collapse',)
+                     'classes': ('collapse wide extrapretty',)
                     }),
     )
 admin.site.register(models.translate,TranslateAdmin)
