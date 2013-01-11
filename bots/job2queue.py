e7620231cb85734def77d11980f05884670fbbc8
hjebbers <hjebbers@gmail.com>
2013-01-11 14:03:30 +0100

    fix bug: command lien parsing in job2queue

--- bots/job2queue.py
+++ bots/job2queue.py
@@ -32,6 +32,9 @@ def send_job_to_jobqueue(task_args,priority=5):
 def start():
     #NOTE: bots directory should always be on PYTHONPATH - otherwise it will not start.
     #***command line arguments**************************
+    #if config (-c option) is before job argument, will be used as config-dir of job2queue it self and not for job
+    #if config (-c option) is after job argument, use both as config-dir of job2queue and as -c option of job.
+    #if config (-c option) is before and after job argument, use only the after...could change that but seems not to be useful.
     usage = '''
     This is "%(name)s" version %(version)s, part of Bots open source edi translator (http://bots.sourceforge.net).
     Places a job in the bots jobqueue. Bots jobqueue takes care of correct processing of jobs.    
@@ -42,34 +45,32 @@ def start():
         -p<priority>    priority of job, 1-9 (default: 5, highest priority is 1).
     Example of usage:
         %(name)s bots-engine.py
-        %(name)s -p5 python2.7 /usr/local/bin/bots-engine.py
-        %(name)s -cconfig2 python2.7 /usr/local/bin/bots-engine.py -cconfig2 myroute
+        %(name)s python2.7 /usr/local/bin/bots-engine.py
+        %(name)s -p1 python2.7 /usr/local/bin/bots-engine.py -cconfig2 myroute
         
     '''%{'name':os.path.basename(sys.argv[0]),'version':botsglobal.version}
     configdir = 'config'    #default value
     priority = 5            #default value
     task_args = []
     for arg in sys.argv[1:]:
-        if not task_args:
-            if arg.startswith('-c'):
-                configdir = arg[2:]
-                if not configdir:
-                    print 'Error: configuration directory indicated, but no directory name.'
-                    sys.exit(1)
-            elif arg.startswith('-p'):
-                try:
-                    priority =  int(arg[2:])
-                except:
-                    print 'Error: priority should be numeric (1=highest, 9=lowest).'
-                    sys.exit(1)
-            elif arg in ["?", "/?",'-h', '--help']:
-                print usage
-                sys.exit(0)
-            else:
+        if arg.startswith('-c'):
+            configdir = arg[2:]
+            if not configdir:
+                print 'Error: configuration directory indicated, but no directory name.'
+                sys.exit(1)
+            if task_args:
                 task_args.append(arg)
+        elif arg.startswith('-p'):
+            try:
+                priority =  int(arg[2:])
+            except:
+                print 'Error: priority should be numeric (1=highest, 9=lowest).'
+                sys.exit(1)
+        elif arg in ["?", "/?",'-h', '--help']:
+            print usage
+            sys.exit(0)
         else:
             task_args.append(arg)
-    task_args.append(configdir)
     #***end handling command line arguments**************************
     botsinit.generalinit(configdir)         #needed to read config
     if not botsglobal.ini.getboolean('jobqueue','enabled',False):
