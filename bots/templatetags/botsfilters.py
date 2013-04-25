28f7ade9ecdba843e88b2a1cc8d67c0cdfdc825a
hjebbers <hjebbers@gmail.com>
2013-04-26 00:29:41 +0200

    Issue 218: 	improve: incoming, outgoing and detail screen - in incoming screen, when 'several values': link to detail screen. - short filenames (eg /...path/filename). Pop-up shows full name. - re-arranged fields.

--- bots/templatetags/botsfilters.py
+++ bots/templatetags/botsfilters.py
@@ -1,4 +1,15 @@
+import os
 from django import template
 #~ from bots import botsglobal
 
 register = template.Library()
+
+# Sets the minimum selectable date in datetimepicker widget
+@register.filter
+def shortpath(path):
+    if len(path) <= 25:
+        return path
+    elif os.path.isabs(path):
+        return path[:1] + u'\u2026' + path[-23:]
+    else:
+        return path[:6] + u'\u2026' + path[-18:]
