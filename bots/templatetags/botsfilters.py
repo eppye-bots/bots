fefbba94e948701c83b69d2d0d3490084f6b5cfd
hjebbers <hjebbers@gmail.com>
2014-03-03 16:12:32 +0100

    bug for change: in GUI, filename was not always displayed.

--- bots/templatetags/botsfilters.py
+++ bots/templatetags/botsfilters.py
@@ -5,8 +5,10 @@ register = template.Library()
 
 @register.filter
 def shortpath(path):
-    terug = os.path.basename(path)
-    if terug:
-        return terug
-    else:
-        return '(file)'     #for soem cases there is no good filename.....
+    if path:
+        terug = os.path.basename(path)
+        if terug:
+            return terug
+        else:
+            return '(file)'     #for soem cases there is no good filename.....
+    return ''
