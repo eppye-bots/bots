159af9592599a0d174b53ca0c3e1c46f629785bb
hjebbers <hjebbers@gmail.com>
2014-05-27 18:03:44 +0200

    bug fix for  Issue 252: 	migrate to django 1.5-1.6-1.7 (password change gave errors)Issue 252: 	migrate to django 1.5-1.6-1.7

--- bots/urls.py
+++ bots/urls.py
@@ -1,5 +1,5 @@
 #~ from django.conf.urls.defaults import patterns,include   #depreciated in 1.5
-from django.conf.urls import patterns,include           #does not work in django 1.3
+from django.conf.urls import patterns,include,url           #does not work in django 1.3
 from django.contrib import admin
 #~ from django.views.generic.base import RedirectView
 from django.contrib.auth.decorators import login_required,user_passes_test
@@ -11,36 +11,36 @@ superuser_required = user_passes_test(lambda u: u.is_superuser)
 run_permission = user_passes_test(lambda u: u.has_perm('bots.change_mutex'))
 
 urlpatterns = patterns('',
-    (r'^login.*', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-    (r'^logout.*', 'django.contrib.auth.views.logout',{'next_page': '/'}),
-    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
-    (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
+    url(r'^login.*', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+    url(r'^logout.*', 'django.contrib.auth.views.logout',{'next_page': '/'}),
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done',name='password_change_done'),
     #login required
-    (r'^home.*', login_required(views.home)),
-    (r'^incoming.*', login_required(views.incoming)),
-    (r'^detail.*', login_required(views.detail)),
-    (r'^process.*', login_required(views.process)),
-    (r'^outgoing.*', login_required(views.outgoing)),
-    (r'^document.*', login_required(views.document)),
-    (r'^reports.*', login_required(views.reports)),
-    (r'^confirm.*', login_required(views.confirm)),
-    (r'^filer.*', login_required(views.filer)),
-    (r'^srcfiler.*', login_required(views.srcfiler)),
+    url(r'^home.*', login_required(views.home)),
+    url(r'^incoming.*', login_required(views.incoming)),
+    url(r'^detail.*', login_required(views.detail)),
+    url(r'^process.*', login_required(views.process)),
+    url(r'^outgoing.*', login_required(views.outgoing)),
+    url(r'^document.*', login_required(views.document)),
+    url(r'^reports.*', login_required(views.reports)),
+    url(r'^confirm.*', login_required(views.confirm)),
+    url(r'^filer.*', login_required(views.filer)),
+    url(r'^srcfiler.*', login_required(views.srcfiler)),
     #only staff
-    (r'^admin/$', login_required(views.home)),  #do not show django admin root page
-    (r'^admin/bots/$', login_required(views.home)),  #do not show django admin root page
-    (r'^admin/', include(admin.site.urls)),
-    (r'^runengine.+', run_permission(views.runengine)),
+    url(r'^admin/$', login_required(views.home)),  #do not show django admin root page
+    url(r'^admin/bots/$', login_required(views.home)),  #do not show django admin root page
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^runengine.+', run_permission(views.runengine)),
     #only superuser
-    (r'^delete.*', superuser_required(views.delete)),
-    (r'^plugin/index.*', superuser_required(views.plugin_index)),
-    (r'^plugin.*', superuser_required(views.plugin)),
-    (r'^plugout/index.*', superuser_required(views.plugout_index)),
-    (r'^plugout/backup.*', superuser_required(views.plugout_backup)),
-    (r'^plugout.*', superuser_required(views.plugout)),
-    (r'^sendtestmail.*', superuser_required(views.sendtestmailmanagers)),
+    url(r'^delete.*', superuser_required(views.delete)),
+    url(r'^plugin/index.*', superuser_required(views.plugin_index)),
+    url(r'^plugin.*', superuser_required(views.plugin)),
+    url(r'^plugout/index.*', superuser_required(views.plugout_index)),
+    url(r'^plugout/backup.*', superuser_required(views.plugout_backup)),
+    url(r'^plugout.*', superuser_required(views.plugout)),
+    url(r'^sendtestmail.*', superuser_required(views.sendtestmailmanagers)),
     #catch-all
-    (r'^.*', 'bots.views.index'),
+    url(r'^.*', 'bots.views.index'),
     )
 
 handler500 = 'bots.views.server_error'
